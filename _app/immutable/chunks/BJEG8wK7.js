fa13417080c8173fc2f3bc2902260b0f0bc2236b
Pranav Agarkar <pranavagarkar8@gmail.com>
2026-01-02 18:37:01 +0530

    Updates

---
+++ _app/immutable/chunks/BJEG8wK7.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_l6ol2w?.base??"/portfolio-svelte",l=globalThis.__sveltekit_l6ol2w?.assets??s??"";export{l as a,s as b};
--- _app/immutable/chunks/Cd_86nD2.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1kgznlq?.base??"/portfolio-svelte",e=globalThis.__sveltekit_1kgznlq?.assets??s??"";export{e as a,s as b};
